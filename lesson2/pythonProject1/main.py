366d5a6ca199e5c2a93268d07cca6ae7d5e7dac9
mrxmrx16 <97172176+mrxmrx16@users.noreply.github.com>
2024-09-09 18:52:54 +0400

    Update main.py

--- lesson2/pythonProject1/main.py
+++ lesson2/pythonProject1/main.py
@@ -1,5 +1,5 @@
 the_number_of_completed_DZ = 12
 the_number_of_hours_spent = 1.5
 course_name = 'Python'
-time_per_task = the_number_of_completed_DZ / the_number_of_hours_spent
+time_per_task = the_number_of_hours_spent / the_number_of_completed_DZ
 print( 'Курс:', course_name ,', всего задач:', the_number_of_completed_DZ ,', затрачено часов:', the_number_of_hours_spent, ', среднее время выполнения ', time_per_task, ' часа.', sep='' )
